a8170835e003367bc634dd287bc4744bef816419
Dave Dong <fudaviddong@gmail.com>
2017-09-29 15:20:12 -0700

    Initial Commit

---
+++ Assets/_Core/Clock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game.Core{
+	public class Clock : MonoBehaviour {
+		[SerializeField] float _timer = 0;
+
+		void Update()
+		{
+			_timer = _timer + Time.deltaTime;
+		}
+
+	}
+
+}
