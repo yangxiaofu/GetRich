158f431c91f227cf80e0df77c609e8e6d33c285c
Dave Dong <fudaviddong@gmail.com>
2017-09-30 08:00:57 -0700

    Added Character into the scene.

---
+++ Assets/_Characters/Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+namespace Game.Characters{
+	public class Character : MonoBehaviour {
+		AICharacterControl _aiCharacterControl;
+
+		void Start(){
+			_aiCharacterControl = GetComponent<AICharacterControl>();
+
+
+			_aiCharacterControl.SetTarget(GameObject.Find("Walk Target").transform);
+		}
+	}
+
+}
