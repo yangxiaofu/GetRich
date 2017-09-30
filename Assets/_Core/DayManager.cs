17b41adb553c6966fcd92330f6104d9690341faf
Dave Dong <fudaviddong@gmail.com>
2017-09-29 21:52:44 -0700

    Task UI

--- Assets/_Core/DayManager.cs
+++ Assets/_Core/DayManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 namespace Game.Core{
 	[RequireComponent(typeof(Clock))]
 	public class DayManager : MonoBehaviour {
+		[SerializeField] List<Task> _tasksForTheDay = new List<Task>();
+		public List<Task> GetTasksForTheDay(){
+			return _tasksForTheDay;
+		}
 		[SerializeField] int _currentDay = 0;
 		public int currentDay{
 			get{return _currentDay;}
